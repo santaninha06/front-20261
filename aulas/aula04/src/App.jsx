e5bd0d84e89fa4a689fc95ad5da792973f447b7d
unknown <2514290017@ACADEMICO.IESB.BR>
2026-05-05 10:06:29 -0300

    Adionando codigos da aula04

--- aulas/aula04/src/App.jsx
+++ aulas/aula04/src/App.jsx
@@ -2,6 +2,7 @@ import {Routes,Route} from 'react-router';
 import Layout from './layouts/Layout';
 import About from './pages/About';
 import Home from './pages/Home';
+import Login from './pages/Login';
 import Perfil from './pages/Perfil';
 import Settings from './pages/Settings';
 
@@ -14,6 +15,7 @@ function App(){
 <Route path="/perfil" element={<Perfil/>}/>
 <Route path="/about" element={<About/>}/>
   </Route>
+  <Route path ="login" element={<Login />} />
   </Routes>
   );
 }
