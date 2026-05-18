695c3774d8fe05b6777ecc64d8d1d225556fe30f
unknown <2514290017@ACADEMICO.IESB.BR>
2026-05-18 10:04:46 -0300

    feat: conclui pratica06. Fecha #6

--- praticas/aluno-online-react/src/App.jsx
+++ praticas/aluno-online-react/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router";
 import Layout from "./components/Layout";
 import Dashboard from "./pages/Dashboard";
 import Faltas from "./pages/Faltas";
