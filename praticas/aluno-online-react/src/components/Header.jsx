afb8df1547b1b286ef100d147ac9916cafadd667
unknown <2514290017@ACADEMICO.IESB.BR>
2026-05-20 09:42:21 -0300

    feat: conclui pratica07. Fecha #16

--- praticas/aluno-online-react/src/components/Header.jsx
+++ praticas/aluno-online-react/src/components/Header.jsx
@@ -1,12 +1,25 @@
-import '../components/Header.css';
+import "../components/Header.css";
+
+import { useAuth } from "../contexts/AuthContext";
 
 function Header({ titulo }) {
+
+  const { usuario } = useAuth();
+
   return (
     <header>
-      <h1>{titulo}</h1>
-      <img 
+
+      <div>
+        <h1>{titulo}</h1>
+
+        <p>{usuario.nome}</p>
+      </div>
+
+      <img
         src="https://raw.githubusercontent.com/santaninha06/front-20261/1a5194dd05e434768284274cb75e333c00765664/praticas/aluno-online-react/avatar.svg"
-        alt="Avatar do aluno"/>
+        alt="Avatar do aluno"
+      />
+
     </header>
   );
 }
