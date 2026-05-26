b5772b3927a978bd2ee54d7a73e18a7f6f24d9a4
unknown <2514290017@ACADEMICO.IESB.BR>
2026-05-26 10:39:40 -0300

    desfazendo pull request

--- praticas/aluno-online-vanilla/stylelint.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  extends: ["stylelint-config-standard"],
-  rules: {
-    "color-hex-length": "short",
-    "font-family-no-missing-generic-family-keyword": null,
-    "no-descending-specificity": null,
-    "no-empty-source": null
-  }
-};
