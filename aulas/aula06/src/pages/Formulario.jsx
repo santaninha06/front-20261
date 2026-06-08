8995d63bb76e069dc6608b36fb63bd17c8a873fb
unknown <2514290017@ACADEMICO.IESB.BR>
2026-06-08 10:09:37 -0300

    Atualização dos codigos

--- aulas/aula06/src/pages/Formulario.jsx
+++ aulas/aula06/src/pages/Formulario.jsx
@@ -1,54 +1,64 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
-import { Link, useNavigate } from "react-router";
-import { criar } from "../services/produtoService";
+import { Link, useNavigate, useParams } from "react-router";
+import { atualizar, criar, obter } from "../services/produtoService";
 
 function Formulario() {
-    const [erro, setErro] = useState();
-    const navigate = useNavigate();
+  const [erro, setErro] = useState();
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const { register, handleSubmit, reset } = useForm();
 
-    const { register, handleSubmit } = useForm();
+  const salvar = async (dados) => {
+    try {
+      if (id) {
+        await atualizar({ id, ...dados });
+      } else {
+        await criar(dados);
+      }
+      navigate("/produtos");
+    } catch (error) {
+      setErro(error.message);
+    }
+  };
 
-    const salvar = async (dados) => {
-        try {
-            await criar(dados);
-            navigate("/produtos");
-        } catch (error) {
-            setErro(error.message);
-        }
-    };
+  useEffect(() => {
+    if (!id) {
+      return;
+    }
 
-    return (
-        <>
-            <h1>Cadastro de Produtos</h1>
+    const disparar = async () => {
+      const resposta = await obter({id});
+      reset(resposta);
+    }
 
-            <p>{erro}</p>
+    disparar();
+  }, []);
 
-            <form onSubmit={handleSubmit(salvar)}>
-                <input
-                    type="text"
-                    placeholder="Nome do Produto"
-                    {...register("nome")}
-                />
-
-                <input
-                    type="number"
-                    placeholder="Preço 0.00"
-                    {...register("preco")}
-                />
-
-                <input
-                    type="text"
-                    placeholder="Unidade"
-                    {...register("unidade")}
-                />
-
-                <Link to="/produtos">Cancelar</Link>
-
-                <button type="submit">Salvar</button>
-            </form>
-        </>
-    );
+  return (
+    <>
+      <h1>Cadastro de Produtos</h1>
+      <p>{erro}</p>
+      <form onSubmit={handleSubmit(salvar)}>
+        <input
+          type="text"
+          placeholder="Nome do Produto"
+          {...register("nome")}
+        />
+        <input 
+          type="text" 
+          placeholder="Preço 0,00" 
+          {...register("preco")} />
+        <input 
+          type="text"
+          placeholder="Unidade" 
+          {...register("unidade")} 
+        />
+        <Link to="/produtos">Cancelar</Link>
+        <button type="submit">Salvar</button>
+      </form>
+    </>
+  );
 }
 
 export default Formulario;
--- aulas/aula06/src/pages/Listagem.jsx
+++ aulas/aula06/src/pages/Listagem.jsx
@@ -1,54 +1,53 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router";
-import { listar } from "../services/produtoServices";
+import { Link, useNavigate } from "react-router";
+import { listar, remover } from "../services/produtoService";
 
 function Listagem() {
-    const [dados, setDados] = useState([]);
-    const navigate = useNavigate();
+  const [dados, setDados] = useState([]);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const carregar = async () => {
-            const resposta = await listar();
-            setDados(resposta);
-        };
+  const trataRemover = async (produto) => {
+     await remover(produto);
+     setDados(dados.filter((item) => item.id != produto.id));
+  }
 
-        carregar();
-    }, []);
+  useEffect(() => {
+    const carregar = async () => {
+      const resposta = await listar();
+      setDados(resposta);
+    };
+    carregar();
+  }, []);
 
-    return (
-        <>
-            <h1>Listagem de Produtos</h1>
-
-            <button onClick={() => navigate("/novo")}>
-                Novo
-            </button>
-
-            <table>
-                <thead>
-                    <tr>
-                        <th>Nome do Produto</th>
-                        <th>Preço</th>
-                        <th>Unidade</th>
-                        <th>Ações</th>
-                    </tr>
-                </thead>
-
-                <tbody>
-                    {dados.map((item) => (
-                        <tr key={item.id}>
-                            <td>{item.nome}</td>
-                            <td>{item.preco}</td>
-                            <td>{item.unidade}</td>
-                            <td>
-                                <Link to={`/produtos/editar/${item.id}`}>Editar</Link>
-                                <Link to="/produtos">remover </Link>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </>
-    );
+  return (
+    <>
+      <h1>Listagem de Produtos</h1>
+      <button onClick={() => navigate("/produtos/novo")}>Novo</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Nome do Produto</th>
+            <th>Preço</th>
+            <th>Unidade</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {dados.map((item) => (
+            <tr key={item.id}>
+              <td>{item.nome}</td>
+              <td>{item.preco}</td>
+              <td>{item.unidade}</td>
+              <td>
+                <Link to={`/produtos/editar/${item.id}`}>Editar</Link>|
+                <Link to="/produtos" onClick={() => trataRemover(item)}>Remover</Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 }
 
 export default Listagem;
