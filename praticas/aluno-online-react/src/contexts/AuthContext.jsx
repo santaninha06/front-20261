c6ee8df5c216241e59ccf934c198eaac9051905a
unknown <2514290017@ACADEMICO.IESB.BR>
2026-06-09 10:49:43 -0300

    corrigindo erro part:4

--- praticas/aluno-online-react/src/contexts/AuthContext.jsx
+++ praticas/aluno-online-react/src/contexts/AuthContext.jsx
@@ -1,35 +1,37 @@
-import { createContext, useContext, useState } from "react";
+ /* eslint-disable react-refresh/only-export-components */
 
-const AuthContext = createContext();
+ import { createContext, useContext, useState } from "react";
 
-export function AuthProvider({ children }) {
-  const [autenticado, setAutenticado] = useState(false);
-  const [usuario, setUsuario] = useState(null);
-
-  const login = (dadosUsuario) => {
-    setUsuario(dadosUsuario);
-    setAutenticado(true);
-  };
-
-  const logout = () => {
-    setUsuario(null);
-    setAutenticado(false);
-  };
-
-  return (
-    <AuthContext.Provider
-      value={{
-        autenticado,
-        usuario,
-        login,
-        logout,
-      }}
-    >
-      {children}
-    </AuthContext.Provider>
-  );
-}
-
-export function useAuth() {
-  return useContext(AuthContext);
-}
+ const AuthContext = createContext();
+ 
+ export function AuthProvider({ children }) {
+   const [autenticado, setAutenticado] = useState(false);
+   const [usuario, setUsuario] = useState(null);
+ 
+   const login = (dadosUsuario) => {
+     setUsuario(dadosUsuario);
+     setAutenticado(true);
+   };
+ 
+   const logout = () => {
+     setUsuario(null);
+     setAutenticado(false);
+   };
+ 
+   return (
+     <AuthContext.Provider
+       value={{
+         autenticado,
+         usuario,
+         login,
+         logout,
+       }}
+     >
+       {children}
+     </AuthContext.Provider>
+   );
+ }
+ 
+ export function useAuth() {
+   return useContext(AuthContext);
+ }
